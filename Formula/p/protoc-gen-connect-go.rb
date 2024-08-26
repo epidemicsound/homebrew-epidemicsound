6003149001ad8a2c77782e29fc193b46b024584b
Daniel Petersson <daniel.petersson@epidemicsound.com>
2024-08-26 17:16:08 +0200

    remove some comments from the protoc-gen-connect-go formula

--- Formula/p/protoc-gen-connect-go.rb
+++ Formula/p/protoc-gen-connect-go.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class ProtocGenConnectGo < Formula
   desc "The Go implementation of Connect: Protobuf RPC that works."
   homepage "https://connectrpc.com"
